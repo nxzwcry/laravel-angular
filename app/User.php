e89a78b982dd3790acaf6d07c8361dd0548b9344
nxzwcry <nxzwcry@163.com>
2018-03-06 17:10:32 +0800

    api oath成功

--- app/User.php
+++ app/User.php
@@ -2,12 +2,13 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +27,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * 自定义用Passport授权登录：邮箱+密码
+     * @param $username
+     * @return mixed
+     */
+    public function findForPassport($email)
+    {
+        return self::where('email', $email)->first();
+    }
 }
